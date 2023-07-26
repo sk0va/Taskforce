a22f8e5903efaeddeebf1b788a646f011c8a7564
Sergii <serg.contacts@gmail.com>
2023-07-26 15:03:37 +0300

    Minor refactoring of DI registrations

--- src/Taskforce.App/DependenciesRegistrar.cs
+++ src/Taskforce.App/DependenciesRegistrar.cs
@@ -12,25 +12,29 @@ using Skova.Repository.DependencyInjection;
 
 namespace Taskforce.App;
 
-internal static class DependenciesRegistrator
+internal static class DependenciesRegistrar
 {
     public static void RegisterServices(IServiceCollection services)
     {
-        services.AddTransient<ICommandLauncher, CommandLauncher>();
-
-        services.AddScoped<ICommandHandler<CreateTaskCommand>, CreateTaskCommandHandler>();
-        services.AddScoped<ICommandHandler<UpdateTaskCommand>, UpdateTaskCommandHandler>();
-
-        services.AddScoped<ICommandHandler<CreateEventCommand>, CreateEventCommandHandler>();
-        services.AddScoped<ICommandHandler<UpdateEventCommand>, UpdateEventCommandHandler>();
-
-        services.AddScoped(typeof(IEntityQuery<>), typeof(EntityQuery<>));
-
         services.AddUnitOfWorkAsScoped<TaskforceDbContext>()
             .AddRepositoryAsScoped<Domain.Tasks.Task, DbTask>()
             .AddSpecificationAsTransient<ITaskSpecification, TaskSpecification>()
 
             .AddRepositoryAsScoped<Event, DbEvent>()
             .AddSpecificationAsTransient<IEventSpecification, EventSpecification>();
+
+        services.AddTransient<ICommandLauncher, CommandLauncher>();
+        services.AddScoped(typeof(IEntityQuery<>), typeof(EntityQuery<>));
+
+        void AddCommandHandler<TCommand, THandler>()
+            where TCommand : ICommand
+            where THandler: class, ICommandHandler<TCommand>
+            => services.AddScoped<ICommandHandler<TCommand>, THandler>();
+
+        AddCommandHandler<CreateTaskCommand, CreateTaskCommandHandler>();
+        AddCommandHandler<UpdateTaskCommand, UpdateTaskCommandHandler>();
+
+        AddCommandHandler<CreateEventCommand, CreateEventCommandHandler>();
+        AddCommandHandler<UpdateEventCommand, UpdateEventCommandHandler>();
     }
 }
